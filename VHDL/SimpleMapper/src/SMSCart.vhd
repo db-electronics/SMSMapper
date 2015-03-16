977458f47af45db702e39940c244bebb744fe97d
René Richard <rene@db-elec.com>
2015-03-16 12:13:16 -0400

    Changed to Sega Mapper only for testing

--- VHDL/SimpleMapper/src/SMSCart.vhd
+++ VHDL/SimpleMapper/src/SMSCart.vhd
@@ -7,30 +7,23 @@
 --
 --*************************************************************
 --  db Mapper
---  Copyright 2014 Rene Richard
+--  Copyright 2015 Rene Richard
 --  DEVICE : EPM3064ATC100-10
 --*************************************************************
 --
 --  Description:
---		This is a VHDL implementation of a hybrid Sega + Codemasters SMS ROM mapper - the db Mapper
+--		This is a VHDL implementation of an SMS Sega Mapper
 --		it is intended to be used on the db Electronics SMS Homebrew Carts
 --		Supported Flash Memory Configurations:
---			1Mbit (1x 1Mbit)
 --			2Mbit (1x 2Mbit)
 --			4Mbit (1x 4Mbit)
---			8Mbit (2x 4Mbit)
+--			8Mbit (1x 4Mbit)
 --		Support RAM Configurations
 --			32KB
 --
 --		for a complete description of SMS Mappers, go to http://www.smspower.org/Development/Mappers
 --*************************************************************
 --
---	Hybridization Features:
---		Register Merging:
---			$0000 (CM) merged with $FFFD (SEGA)
---			$4000 (CM) merged with $FFFE (SEGA)
---			$8000 (CM) merged with $FFFF (SEGA)
---			both addresses are valid
 --		RAM and Misc. Register
 --			$FFFC
 --				bit 7: ROM Write Enable
@@ -56,18 +49,24 @@ entity SMSCart is
 
 	port (
 			--input from sms
-			ADDR_p		:	in		std_logic_vector(15 downto 0);
-			DATA_p		:	in 	std_logic_vector(7 downto 0);
-			nRST_p		:	in 	std_logic;
-			nWR_p			:	in		std_logic;
-			nMREQ_p		:	in		std_logic;
+			ADDR_p			:	in		std_logic_vector(15 downto 0);
+			DATA_p			:	in 	std_logic_vector(7 downto 0);
+			nRST_p			:	in 	std_logic;
+			nWR_p				:	in		std_logic;
+			nCE_p				:	in		std_logic;
+			nOE_p				:	in		std_logic;
 			
 			--output to ROM
-			nROMWR_p			:	out	std_logic;
-			nROMCE0_p		:	out	std_logic;
-			nROMCE1_p		:	out 	std_logic;
-			ROMADDR1814_p	:	out	std_logic_vector(4 downto 0);
+			nROMWE_p			:	out	std_logic;
+			nROMCE_p			:	out	std_logic;
+			ROMADDR1914_p	:	out	std_logic_vector(5 downto 0);
 
+			--out to serial EEPROM
+			EE_CS_p			: 	out	std_logic;
+			EE_SO_p			:	out	std_logic;
+			EE_SI_p			:	out	std_logic;
+			EE_SCK_p			:	out	std_logic;
+			
 			--output to SRAM
 			nSRAMCE_p		:	out	std_logic;
 			SRAMADDR14_p	:	out	std_logic
@@ -79,21 +78,17 @@ architecture SMSCart_a of SMSCart is
 	--internal data and address signals for easy write back if ever needed
 	signal datain_s		:	std_logic_vector(7 downto 0);
 	signal addr_s			:	std_logic_vector(15 downto 0);
-	signal mapAddr_s		:  std_logic_vector(5 downto 0);
-	signal mapAddrShift_s	:  std_logic_vector(5 downto 0);
 	
 	--Mapper slot registers, fitter will optimize any unused bits
 	signal romSlot0_s		:	std_logic_vector(5 downto 0);
 	signal romSlot1_s		:	std_logic_vector(5 downto 0);
 	signal romSlot2_s		:	std_logic_vector(5 downto 0);
-
-	--Bank Shift
-	signal bankShiftCode_s	:	std_logic_vector(5 downto 0);
-	signal bankShift_s	:	std_logic_vector(3 downto 0);
-	signal bankShiftEn_s	:	std_logic;
+	signal mapAddr_s		: 	std_logic_vector(5 downto 0);
 	
-	--internal write signals
+	--internal rom signals
 	signal romWrEn_s		:	std_logic;
+	signal nRomCE_s		: 	std_logic;
+	signal nRomWE_s		:	std_logic;
 	
 	--RAM mapping signals
 	signal ramEn_s			:	std_logic;
@@ -106,72 +101,54 @@ begin
 	datain_s <= DATA_p;
 	
 	--output mapping to ROM and RAM
-	--	internally it is handled as 1x 1MB, externally it is 2x 512KB chips
-	--chip select to both ROMs handled in chipSelect process below
-	ROMADDR1814_p <= mapAddrShift_s(4 downto 0);
 	SRAMADDR14_p <= ramBank_s;
 	
+	--high order address bits and WE and CE must be open-drain to meet 5V requirements
+	-- 1K pull-up on each of these lines
+	ROMADDR1914_p(0) <= '0' when mapAddr_s(0) = '0' else 'Z';
+	ROMADDR1914_p(1) <= '0' when mapAddr_s(1) = '0' else 'Z';
+	ROMADDR1914_p(2) <= '0' when mapAddr_s(2) = '0' else 'Z';
+	ROMADDR1914_p(3) <= '0' when mapAddr_s(3) = '0' else 'Z';
+	ROMADDR1914_p(4) <= '0' when mapAddr_s(4) = '0' else 'Z';
+	ROMADDR1914_p(5) <= '0' when mapAddr_s(5) = '0' else 'Z';
 	--ROM Write Gating with bit7 of $FFFC
-	nROMWR_p <= nWR_p when romWrEn_s = '1' else '1';
+	nRomWE_s <= nCE_p when romWrEn_s = '1' else 'Z';
+	nROMWE_p <= '0' when nRomWE_s = '0' else 'Z';
+	nROMCE_p <= '0' when nRomCE_s = '0' else 'Z';
+	
+	--default values for now
+	EE_CS_p <= '1';
+	EE_SO_p <= '1';
+	EE_SI_p <= '1';
+	EE_SCK_p <= '1';
 	
 	--RAM mapping and miscellaneous functions register
-	ram0: process( nRST_p, nWR_p, nMREQ_p, addr_s )
+	ram0: process( nRST_p, nWR_p, nCE_p, addr_s )
 	begin
 		if nRST_p = '0' then
 			romWrEn_s <= '0';
 			ramEn_s <= '0';
 			ramBank_s <= '0';
-		elsif rising_edge(nWR_p) then
-			if addr_s = x"FFFC" and  nMREQ_p = '0' then
+		--nWR rises before address and mreq on Z80
+		elsif falling_edge(nWR_p) then
+			if addr_s = x"FFFC" and  nCE_p = '0' then
 				romWrEn_s <= datain_s(7);
 				ramEn_s <= datain_s(3);
 				ramBank_s <= datain_s(2);
-				if bankShiftEn_s = '0' then
-					bankShift_s(3) <= datain_s(6);
-					bankShift_s(2) <= datain_s(5);
-					bankShift_s(1) <= datain_s(1);
-					bankShift_s(0) <= datain_s(0);
-				end if;
-			end if;
-		end if;
-	end process;
-	
-
-	--bank shifting register
-	bankshift: process( nRST_p, nWR_p, nMREQ_p, addr_s)
-	begin
-		if nRST_p = '0' then
-			bankShiftCode_s <= "000000";
-			bankShiftEn_s <= '0';
-		elsif rising_edge(nWR_p) then
-			if nMREQ_p = '0' then 
-				case addr_s is
-					when x"FFFC" =>
-						if bankShiftCode_s = "101101" then
-							bankShiftEn_s <= '0';
-						end if;
-					when x"FFFD" => 
-						bankShiftCode_s(1 downto 0) <= datain_s(7 downto 6);
-					when x"FFFE" =>
-						bankShiftCode_s(3 downto 2) <= datain_s(7 downto 6);
-					when x"FFFF" =>
-						bankShiftCode_s(5 downto 4) <= datain_s(7 downto 6);
-					when others =>
-						null;
-				end case;
 			end if;
 		end if;
 	end process;
 	
 	--mapper registers
-	mappers: process( nRST_p, nWR_p, nMREQ_p, addr_s)
+	mappers: process( nRST_p, nWR_p, nCE_p, addr_s)
 	begin
 		if nRST_p = '0' then
 			romSlot0_s <= "000000";
 			romSlot1_s <= "000001";
 			romSlot2_s <= "000010";
-		elsif rising_edge(nWR_p) then
-			if nMREQ_p = '0' then
+		--nWR rises before address and mreq on Z80
+		elsif falling_edge(nWR_p) then
+			if nCE_p = '0' then
 				case addr_s is
 					when x"FFFD" => 
 						romSlot0_s <= datain_s(5 downto 0);
@@ -208,53 +185,31 @@ begin
 		end case;
 	end process;
 	
-	--apply bankshifting if bankShiftEn_s = '1'
-	--mapAddrShift_s <= (bankShift_s or mapAddr_s(5 downto 2)) & mapAddr_s(1 downto 0) when bankShiftEn_s = '1' else
-	--						mapAddr_s;
-	mapAddrShift_s <= (bankShift_s + mapAddr_s(5 downto 2)) & mapAddr_s(1 downto 0) when bankShiftEn_s = '1' else
-							mapAddr_s;
-	
 	--drive chip select lines
-	chipSelect: process( addr_s, mapAddrShift_s, ramEn_s, nMREQ_p )
+	chipSelect: process( addr_s, ramEn_s, nCE_p )
 	begin
 		nSRAMCE_p <= '1';
-		nROMCE0_p <= '1';
-		nROMCE1_p <= '1';
+		nRomCE_s <= '1';
 		case addr_s(15 downto 14) is
 			--slot 0
 			when "00" =>
-				--select upper or lower ROM based on A19 (mapAddrShift_s(5))
-				if mapAddrShift_s(5) = '0' then
-					nROMCE0_p <= nMREQ_p;
-				else
-					nROMCE1_p <= nMREQ_p;
-				end if;
+				nRomCE_s <= nCE_p;
 			--slot 1
 			when "01" =>
-				--select upper or lower ROM based on A19
-				if mapAddrShift_s(5) = '0' then
-					nROMCE0_p <= nMREQ_p;
-				else
-					nROMCE1_p <= nMREQ_p;
-				end if;
+				nRomCE_s <= nCE_p;
 			--slot 2
 			when "10" =>
 				--RAM mapping has priority in Slot 2
 				if ramEn_s = '1' then
-					nSRAMCE_p <= nMREQ_p;
+					nSRAMCE_p <= nCE_p;
 				else
 					--select upper or lower ROM based on A19
-					if mapAddrShift_s(5) = '0' then
-						nROMCE0_p <= nMREQ_p;
-					else
-						nROMCE1_p <= nMREQ_p;
-					end if;
+					nRomCE_s <= nCE_p;
 				end if;
 			when others =>
 				--don't drive anything in slot 4
 				nSRAMCE_p <= '1';
-				nROMCE0_p <= '1';
-				nROMCE1_p <= '1';
+				nRomCE_s <= '1';
 		end case;
 	end process;
 	
